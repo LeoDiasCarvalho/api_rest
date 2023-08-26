f8e3a5790be8bc2f24a70ae69b65528f5a4daa20
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-08-26 18:27:56 -0300

    Criando a classe Task

--- api_rest/src/main/java/com/leodias/api_rest/models/User.java
+++ api_rest/src/main/java/com/leodias/api_rest/models/User.java
@@ -1,5 +1,9 @@
 package com.leodias.api_rest.models;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -8,47 +12,39 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "user")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
-
-    public interface CreateUser{}
-    public interface UpDateUser{}
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", unique = true)
     private Long id;
 
-    @Column(name = "username", length = 100, nullable = false)
-    @NotNull(groups = CreateUser.class)
-    @NotEmpty(groups = CreateUser.class)
-    @Size(groups = CreateUser.class, min=2, max=100)
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @NotBlank
+    @Size(min=2, max=100)
     private String username;
 
     @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 8, nullable = false)
-    @NotNull(groups = {CreateUser.class, UpDateUser.class})
-    @NotEmpty(groups = {CreateUser.class, UpDateUser.class})
-    @Size(groups = {CreateUser.class, UpDateUser.class}, min= 8, max=8)
+    @NotBlank
+    @Size(min= 8, max=8)
     private String password;
 
-
-    public User() {
-    }
-
-
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
+    @OneToMany(mappedBy = "user")
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private List<Task> tasks = new ArrayList<>();
     
 }
